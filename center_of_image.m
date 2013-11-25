927e87683db72ca17518f11b82f74f42be5cfdf1
xie.shicong@gmail.com <xie.shicong@gmail.com@yourdomain.com>
2013-11-25 22:26:30 +0000

    Simulation

--- center_of_image.m
+++ center_of_image.m
@@ -3,7 +3,8 @@ function [mx,my] = center_of_image(img)
 [M,N] = size(img);
 [X,Y] = meshgrid(1:N,1:M);
 
-cell_mass = sum(img(img > 0));
+cell_mass = sum(img(:));
+
 mx = sum(sum( img.*X ))/cell_mass;
 my = sum(sum( img.*Y ))/cell_mass;
 
